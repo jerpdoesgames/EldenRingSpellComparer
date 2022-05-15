e5a3a3e42c810a1b95fd9f2fc3baf6afb8357bc1
jerp <jerpdoesgames@gmail.com>
2022-05-15 18:29:24 -0500

    add spell data
    Still need to add magicEffectCategory data to staves/seals

--- Spells/data/spellSchools.js
+++ Spells/data/spellSchools.js
@@ -1,70 +1,87 @@
 var spellSchools = [
 	{
 		name: "Stonedigger",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 4
 	},
 	{
 		name: "Full Moon",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 1
 	},
 	{
 		name: "Carian Sword",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 2
 	},
 	{
 		name: "Invisibility",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 12
 	},
 	{
 		name: "Gravity",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 11
 	},
 	{
 		name: "Crystalian",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 5
 	},
 	{
 		name: "Thorn",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 9
 	},
 	{
 		name: "Death",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 10
 	},
 	{
 		name: "Glintblade",
-		type: SPELL_TYPE_SORCERY
+		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 3
 	},
 	{
 		name: "Lava",
 		type: SPELL_TYPE_SORCERY,
+        magicEffectCategory: 8
 	},
 	{
 		name: "Golden Order",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 24
 	},
 	{
 		name: "Dragon Cult",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 22
 	},
 	{
 		name: "Fire Monk/Giant",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 21
 	},
 	{
 		name: "Bestial",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 23
 	},
 	{
 		name: "Frenzied Flame",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 26
 	},
 	{
 		name: "Dragon Communion",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 25
 	},
 	{
 		name: "Godskin Apostle",
-		type: SPELL_TYPE_INCANTATION
+		type: SPELL_TYPE_INCANTATION,
+        magicEffectCategory: 20
 	},
 ];
